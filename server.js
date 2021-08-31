74806a4c5aed4e08f6cb5678588b8a3c5b2847ee
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-08-31 04:37:19 -0400

    something

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = 8080
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send({data: 'hello'});
+})
+
 app.get('/validate-token', async (req, res) => {
     const response = await checkIfAuthenticated(req, res);
     res.send(response);
